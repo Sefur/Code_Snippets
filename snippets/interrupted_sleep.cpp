4e67021a20decc98b0506c0f46dd918ed2897113
Sefur <nightsefur@163.com>
2024-06-05 09:36:48 +0800

    [fix] fix mutil call interrupted sleep invalid

--- snippets/interrupted_sleep.cpp
+++ snippets/interrupted_sleep.cpp
@@ -16,6 +16,8 @@ public:
     void Sleep(uint64_t ms)
     {
         auto end_time = std::chrono::steady_clock::now() + std::chrono::milliseconds(ms);        
+	m_Interp.store(false);
+
         while (!m_Interp.load()) {
 
             std::unique_lock<std::mutex> lock(m_Mtx);
@@ -85,6 +87,45 @@ void TEST_INTERPSLEEP(uint64_t sleep_ms, uint64_t realy_ms)
 }
 
 
+void TEST_MULTI_CALL(uint64_t sleep_ms, int times)
+{
+    InterruptedSleep inter_sleep;
+
+    int delta = sleep_ms / times;
+    int realy_ms = delta;
+
+    for (int i = 0; i < times; ++i) {
+    	auto ti = std::chrono::high_resolution_clock::now();
+
+    	auto t_sleep = std::thread([&inter_sleep, sleep_ms](){
+       	    auto start = std::chrono::high_resolution_clock::now();
+            inter_sleep.Sleep(sleep_ms);
+            auto end = std::chrono::high_resolution_clock::now();
+
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+            std::cout << "set sleep time:" << sleep_ms << "ms, really sleep time:" << duration.count() << " ms\n" ;
+        });
+
+    	if (realy_ms != sleep_ms) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(realy_ms));
+            inter_sleep.CancelSleep(); 
+        }
+        t_sleep.join();
+
+        auto to = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(to - ti);
+        if (duration.count() == realy_ms) {
+            std::cout <<"[  ok]----------------------------------\n";
+        }
+        else {
+            std::cout <<"[fail]----------------------------------\n";
+            assert(0);
+        }
+
+	realy_ms += delta;
+    }
+}
+
 
 int main()
 {
@@ -98,5 +139,8 @@ int main()
     TEST_INTERPSLEEP(5000, 2010);
     TEST_INTERPSLEEP(5000, 3100);
     TEST_INTERPSLEEP(5000, 4999);
+
+    TEST_MULTI_CALL(5000, 10); 
+    TEST_MULTI_CALL(5000, 20);
     return 0;
 }
